97c238b7c9fd29b91f1187d7c8c1cc8fcfb464e2
Richard <rstaszki@gitlab-stud.elka.pw.edu.pl>
2021-04-19 00:03:54 +0200

    In main implemented primitive find test

--- Drzewa_CPP/main.cpp
+++ Drzewa_CPP/main.cpp
@@ -10,5 +10,11 @@ int main()
     root.add_value(45);
     root.add_value(86);
     root.print_as_root(0);
+    BST_node result = root.find_value(11);
+    if(result.parent == nullptr) std::cout << "Not in key values\n";
+    else std::cout << "Parent is node with value: " << result.parent->node_value <<'\n';
+    result = root.find_value(-13);
+    if(result.parent == nullptr) std::cout << "Not in key values\n";
+    else std::cout << "Parent is node with value: " << result.parent->node_value <<'\n';
     return 0;
 }
