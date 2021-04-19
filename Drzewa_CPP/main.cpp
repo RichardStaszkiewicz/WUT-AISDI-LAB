8b749e9e7cc578a92d9d25510b3c12108e0ee199
Richard <rstaszki@gitlab-stud.elka.pw.edu.pl>
2021-04-19 22:16:38 +0200

    All tests implemented

--- Drzewa_CPP/main.cpp
+++ Drzewa_CPP/main.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include "AVL.hpp"
-// #include "BST.hpp"
+#include "BST.hpp"
 
 int main()
 {
-    // BST_node root{56};
-    // root.add_value(32);
-    // root.add_value(11);
-    // root.add_value(45);
-    // root.add_value(86);
-    // root.add_value(56);
-    // root.add_value(56);
-    // root.print_as_root(0);
-    // BST_node root2{12};
-    // root2.add_value(19);
-    // root2.add_value(-11);
-    // root2.add_value(19);
-    // root2.print_as_root(0);
-    // if(!root.find_value(11)) std::cout << "Not in key values\n";
-    // else std::cout << "Key in a tree";
-    // if(!root.find_value(-13)) std::cout << "Not in key values\n";
-    // else std::cout << "Key in a tree";
+    BST_node root{56};
+    root.add_value(32);
+    root.add_value(11);
+    root.add_value(45);
+    root.add_value(86);
+    root.add_value(56);
+    root.add_value(56);
+    root.print_as_root(0);
+    BST_node root2{12};
+    root2.add_value(19);
+    root2.add_value(-11);
+    root2.add_value(19);
+    root2.print_as_root(0);
+    if(!root.find_value(11)) std::cout << "Not in key values\n";
+    else std::cout << "Key in a tree";
+    if(!root.find_value(-13)) std::cout << "Not in key values\n";
+    else std::cout << "Key in a tree";
     TreeNode *root3 = NULL;
     root3 = add_value(root3, 10);
-    root3 = add_value(root3, 271);
-    root3 = add_value(root3, 2);
+    root3 = add_value(root3, 12);
+    root3 = add_value(root3, 11);
     root3 = add_value(root3, 21);
-    root3 = add_value(root3, 18);
+    root3 = add_value(root3, 30);
+    // root3 = add_value(root3, 18);
     print_AVL(root3, 0);
 
 
--- Drzewa_CPP/tests/test_AVL.cpp
+++ Drzewa_CPP/tests/test_AVL.cpp
@@ -1,13 +1,166 @@
 #include <gtest/gtest.h>
+#include "AVL.hpp"
 
-TEST(bool_true, true_test)
+
+// problem - obsługa duplikatów w BST
+// ignorowanie duplikatóe w AVL
+
+
+TreeNode *tree1()
 {
-    EXPECT_TRUE(true);
+    TreeNode *root = NULL;
+    root = add_value(root, 10);
+    root = add_value(root, 271);
+    root = add_value(root, 2);
+    root = add_value(root, 21);
+    root = add_value(root, 18);
+    return root;
 }
 
-TEST(bool_false, false_test)
+
+TreeNode *tree2()
 {
-    EXPECT_FALSE(false);
+    TreeNode *root = NULL;
+    for(int i = 0; i < 100; i++) root = add_value(root, i);
+    return root;
+}
+
+
+TreeNode *t1 = tree1();
+TreeNode *t2 = tree2();
+
+
+TEST(test_creating, single_node)
+{
+    TreeNode *root = NULL;
+    root = add_value(root, 5);
+    EXPECT_EQ(root->step, 1);
+    root = add_value(root, 7);
+    EXPECT_EQ(root->right_son->value, 7);
+    EXPECT_EQ(root->right_son->left_son, nullptr);
+    EXPECT_EQ(root->right_son->right_son, nullptr);
+    EXPECT_EQ(root->right_son->step, 1);
+    EXPECT_EQ(root->step, 2);
+}
+
+
+TEST(test_creating, node_simple_aligment)
+{
+    TreeNode *root = NULL;
+    root = add_value(root, 5);
+    root = add_value(root, 7);
+    EXPECT_EQ(root->right_son->value, 7);
+    EXPECT_EQ(root->right_son->left_son, nullptr);
+    EXPECT_EQ(root->right_son->right_son, nullptr);
+    EXPECT_EQ(root->right_son->step, 1);
+    EXPECT_EQ(root->step, 2);
+    root = add_value(root, -9.12);
+    EXPECT_EQ(root->left_son->value, -9.12);
+    EXPECT_EQ(root->left_son->left_son, nullptr);
+    EXPECT_EQ(root->left_son->right_son, nullptr);
+    EXPECT_EQ(root->left_son->step, 1);
+    EXPECT_EQ(root->step, 2);
+    TreeNode *root2 = NULL;
+    root2 = add_value(root2, -1);
+    root2 = add_value(root2, -1);
+    EXPECT_EQ(root2->step, 1);
+    EXPECT_EQ(root2->left_son, nullptr);
+    EXPECT_EQ(root2->right_son, nullptr);
+
+}
+
+
+TEST(test_rotation, left_rotation)
+{
+    TreeNode *root = NULL;
+    root = add_value(root, 5);
+    root = add_value(root, 7);
+    root = add_value(root, 8);
+    EXPECT_EQ(root->value, 7);
+    EXPECT_FALSE(root->right_son == nullptr);
+    EXPECT_FALSE(root->left_son == nullptr);
+    EXPECT_EQ(root->step, 2);
+    EXPECT_EQ(root->right_son->value, 8);
+    EXPECT_EQ(root->right_son->left_son, nullptr);
+    EXPECT_EQ(root->right_son->right_son, nullptr);
+    EXPECT_EQ(root->right_son->step, 1);
+    EXPECT_EQ(root->left_son->value, 5);
+    EXPECT_EQ(root->left_son->left_son, nullptr);
+    EXPECT_EQ(root->left_son->right_son, nullptr);
+    EXPECT_EQ(root->left_son->step, 1);
+}
+
+
+TEST(test_rotation, right_rotation)
+{
+    TreeNode *root = NULL;
+    root = add_value(root, 5);
+    root = add_value(root, 4);
+    root = add_value(root, 3);
+    EXPECT_EQ(root->value, 4);
+    EXPECT_FALSE(root->right_son == nullptr);
+    EXPECT_FALSE(root->left_son == nullptr);
+    EXPECT_EQ(root->step, 2);
+    EXPECT_EQ(root->right_son->value, 5);
+    EXPECT_EQ(root->right_son->left_son, nullptr);
+    EXPECT_EQ(root->right_son->right_son, nullptr);
+    EXPECT_EQ(root->right_son->step, 1);
+    EXPECT_EQ(root->left_son->value, 3);
+    EXPECT_EQ(root->left_son->left_son, nullptr);
+    EXPECT_EQ(root->left_son->right_son, nullptr);
+    EXPECT_EQ(root->left_son->step, 1);
+}
+
+
+TEST(test_rotation, lr_rotation)
+{
+    TreeNode *root = NULL;
+    root = add_value(root, 10);
+    root = add_value(root, 12);
+    root = add_value(root, 11);
+    EXPECT_EQ(root->value, 11);
+    EXPECT_FALSE(root->right_son == nullptr);
+    EXPECT_FALSE(root->left_son == nullptr);
+    EXPECT_EQ(root->step, 2);
+    EXPECT_EQ(root->right_son->value, 12);
+    EXPECT_EQ(root->right_son->left_son, nullptr);
+    EXPECT_EQ(root->right_son->right_son, nullptr);
+    EXPECT_EQ(root->right_son->step, 1);
+    EXPECT_EQ(root->left_son->value, 10);
+    EXPECT_EQ(root->left_son->left_son, nullptr);
+    EXPECT_EQ(root->left_son->right_son, nullptr);
+    EXPECT_EQ(root->left_son->step, 1);
+}
+
+
+TEST(test_rotation, rl_rotation)
+{
+    TreeNode *root = NULL;
+    root = add_value(root, 10);
+    root = add_value(root, 8);
+    root = add_value(root, 9);
+    EXPECT_EQ(root->value, 9);
+    EXPECT_FALSE(root->right_son == nullptr);
+    EXPECT_FALSE(root->left_son == nullptr);
+    EXPECT_EQ(root->step, 2);
+    EXPECT_EQ(root->right_son->value, 10);
+    EXPECT_EQ(root->right_son->left_son, nullptr);
+    EXPECT_EQ(root->right_son->right_son, nullptr);
+    EXPECT_EQ(root->right_son->step, 1);
+    EXPECT_EQ(root->left_son->value, 8);
+    EXPECT_EQ(root->left_son->left_son, nullptr);
+    EXPECT_EQ(root->left_son->right_son, nullptr);
+    EXPECT_EQ(root->left_son->step, 1);
+}
+
+
+TEST(test_find, find)
+{
+    EXPECT_TRUE(find(t1, 21));
+    EXPECT_FALSE(find(t2, 12.21));
+    TreeNode *root = NULL;
+    root = add_value(root, 1.111);
+    EXPECT_FALSE(find(root, 1));
 }
 
 int main(int argc, char **argv)
